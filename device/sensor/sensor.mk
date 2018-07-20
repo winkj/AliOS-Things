833c7ac86b6fe1b8c11d9c29e1150b9cee9415cf
Johannes Winkelmann <jwi@sensirion.com>
2018-07-20 12:21:35 -0700

    add Sensirion SCD30 sensor
    # Conflicts:
    #	device/sensor/hal/sensor_hal.c
    #	device/sensor/sensor.mk
    #	device/sensor/ucube.py

--- device/sensor/sensor.mk
+++ device/sensor/sensor.mk
@@ -13,6 +13,11 @@ $(NAME)_SOURCES += \
         drv/drv_acc_mir3_da217.c \
         drv/drv_als_ps_liteon_ltr553.c \
         drv/drv_temp_humi_sensirion_shtc1.c \
+<<<<<<< HEAD
+=======
+        drv/drv_tvoc_sensirion_sgp30.c \
+        drv/drv_co2_sensirion_scd30.c \
+>>>>>>> 5c430d29... add Sensirion SCD30 sensor
         drv/drv_temp_humi_st_hts221.c \
         drv/drv_mag_st_lis3mdl.c \
         drv/drv_mag_temp_memsic_mmc3680kj.c 
@@ -36,5 +41,6 @@ GLOBAL_DEFINES      += AOS_SENSOR
 #GLOBAL_DEFINES       += AOS_SENSOR_PS_LITEON_LTR553
 #GLOBAL_DEFINES      += AOS_SENSOR_TEMP_SENSIRION_SHTC1
 #GLOBAL_DEFINES      += AOS_SENSOR_HUMI_SENSIRION_SHTC1
+#GLOBAL_DEFINES      += AOS_SENSOR_CO2_SENSIRION_SCD30
 
 
